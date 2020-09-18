8a1580f084e73579983717f6118e8db0c80d055b
hugo matos <hugocfmatos@gmail.com>
2020-09-18 15:10:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length==0) {
     return `The line is currently empty.`
-  }/* else {
-    return `The line is currently ${for (var i==0; i=katzDeliLine.length; i++) { `${i+1}. ${katzDeliLine[i]} `}}`
-  } */
+  } else {
+    var text1 = "The line is currently "
+    for (var i==0; i=katzDeliLine.length; i++) {
+      text1=text1 + (i+1) + ". " + katzDeliLine[i]
+    }
+    return text1
+  }
 }
