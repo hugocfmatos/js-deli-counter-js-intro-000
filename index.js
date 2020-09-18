ce026fc5829e24a72753911243d8e98c8247bc6e
hugo matos <hugocfmatos@gmail.com>
2020-09-18 13:05:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length==0) {
     return `The line is currently empty.`
   } else {
-    return `The line is currently ${for (var i==0; i=katzDeliLine.length; i++) { `${i+1}. ${katzDeliLine[i]} `}}
+    return `The line is currently ${for (var i==0; i=katzDeliLine.length; i++) { `${i+1}. ${katzDeliLine[i]} `}}`
   }
 }
