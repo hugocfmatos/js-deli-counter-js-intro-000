923937163f2e9101431d4f343ec193da1aaaee16
hugo matos <hugocfmatos@gmail.com>
2020-09-18 12:57:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ function nowServing(katzDeliLine) {
   }
 }
 
-/*function currentLine(katzDeliLine) {
-  if katzDeliLine=[] {
+function currentLine(katzDeliLine) {
+  if katzDeliLine.length==0 {
     return `The line is currently empty.`
+  } else {
+    return `The line is currently ${for (var i==0; i=katzDeliLine.length; i++) { `${i+1} ${katzDeliLine[i]} `}
   }
 }
-*/
+
