97d7a4572b1bb9a0edb75ab701237480b71a2113
hugo matos <hugocfmatos@gmail.com>
2020-09-18 12:27:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${katzDeliLine.slice(-1)}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine=[]){
+    return `There is nobody waiting to be served!`
+  } else {
+    return katzDeliLine.shift()
+  }
+}
