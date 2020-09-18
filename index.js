30012fd4855ddccbdda9ec91582f6c1ecae8a73a
hugo matos <hugocfmatos@gmail.com>
2020-09-18 12:46:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length=0){
+  if (katzDeliLine && katzDeliLine.length){
     return `There is nobody waiting to be served!`
   } else {
     return `Currently serving ${katzDeliLine.shift()}`;
